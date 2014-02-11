240c077037e3890ebd41b1f4c5081f06960777b5
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-02-11 23:28:48 +0100

    scaffolding test project

---
+++ FinTsPersistence/FinTsPersistence/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("FinTsPersistence")]
+[assembly: AssemblyCompany("HAUS HOPPE - ITS")]
+[assembly: AssemblyProduct("FinTsPersistence")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.1.*")]
