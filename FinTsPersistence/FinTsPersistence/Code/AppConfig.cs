e6da8d8892bde6dfbf25f117114e634490496bc0
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-05-19 21:29:44 +0200

    work in progress: Searches for suitable app.config file

---
+++ FinTsPersistence/FinTsPersistence/Code/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTsPersistence.Code
+{
+    public static class AppConfig
+    {
+        /// <summary>
+        /// Searches for suitable app.config file an applies its path to the AppDomain
+        /// Will look up into the current directory and will go up recursively, if not found. 
+        /// </summary>
+        public static void Setup()
+        {
+            string currentExecutingDirectory = FolderSearch.CurrentExecutingDirectory();
+            currentExecutingDirectory.FindFileUpwards("FinTsPersistence.exe.config");
+            
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", @"C:\Temp\test.config");
+
+        }
+    }
+}
---
+++ FinTsPersistence/FinTsPersistence/Code/FolderSearch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace FinTsPersistence.Code
+{
+    public static class FolderSearch
+    {
+        private const int MaxLevelOfRecursion = 6;
+
+        public static string CurrentExecutingDirectory()
+        {
+            string filePath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+            return Path.GetDirectoryName(filePath);
+        }
+
+        public static string FindFileUpwards(this string curentPath, string fileName)
+        {
+            return FindFileUpwards(curentPath, fileName, 0);
+        }
+
+        private static string FindFileUpwards(this string curentPath, string fileName, int currentLevel)
+        {
+            if (curentPath == null)
+            {
+                return null;
+            }
+
+            if (currentLevel >= MaxLevelOfRecursion)
+            {
+                return null;
+            }
+
+            string matchingFile = curentPath.FindFile(fileName);
+            return matchingFile ?? curentPath.RemoveLastPart().FindFileUpwards(fileName, currentLevel + 1);
+        }
+
+        public static string FindFile(this string curentPath, string fileName)
+        {
+            string[] files = Directory.GetFiles(curentPath);
+            return files.FirstOrDefault(f => f == fileName);
+        }
+
+        private static string RemoveLastPart(this string path)
+        {
+            if (!path.Contains(@"\"))
+            {
+                return null;
+            }
+
+            List<string> parts = path.Split(new[] {@"\"}, StringSplitOptions.None).ToList();
+            parts.RemoveAt(parts.Count() - 1);
+            return string.Join(@"\", parts.ToArray());
+        }
+    }
+}
